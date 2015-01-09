6c3ae245c5de5e220f18ee398310a7754e5c9335
Matthew R. Scott <matt@11craft.com>
2015-01-09 11:47:51 -0600

    Use Python 3 exception syntax

--- infusionsoft/library.py
+++ infusionsoft/library.py
@@ -12,7 +12,7 @@ class Infusionsoft:
             call = getattr(self.client, service + '.' + method)
             try:
                 return call(self.key, *args)
-            except self.client.error, v:
+            except self.client.error as v:
                 return "ERROR", v
         return function
 
